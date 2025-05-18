e0594f5699121783d14d9e1bdc03a97886928c10
zainabbarambia1 <barambiazainab@gmail.com>
2025-05-19 00:49:38 +0530

    Linkedin post
    https://www.linkedin.com/posts/zainab-barambia-a43a03291_internship-apexplanet-activity-7329940354806321152-smes?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEbOYhMBfiDd4VWAZCQ8jt-MZZBwobJlD1w

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('alertButton');
+    button.addEventListener('click', () => {
+        alert("Thanks for visiting Fashion Fit!");
+    });
+});
